2b29ab3a93ef0cd67decce8a9efc9f39914fea58
ryzhenkov_na <ryzhenkov_na@tgt72.ru>
2025-01-23 13:48:32 +0500

    upd model

--- src/models/statement_model.py
+++ src/models/statement_model.py
@@ -1,5 +1,9 @@
 from src.models.base_model import BaseModel
-
+from sqlalchemy.orm import Mapped, mapped_column
 
 class StatementModel(BaseModel):
-    pass
+    __tablename__ = 'statements'
+
+    name: Mapped[str]
+    email: Mapped[str]
+    phone: Mapped[str]
